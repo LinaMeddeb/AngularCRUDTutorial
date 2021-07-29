73b3891c91bf9a021e384a90c62412a29b775727
Lina Meddeb <lina.meddeb@esprit.tn>
2021-07-29 17:53:51 +0100

    Adding New Article

--- src/app/components/article-new/article-new.component.ts
+++ src/app/components/article-new/article-new.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ArticleService} from "../../services/article.service";
 
 @Component({
   selector: 'app-article-new',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleNewComponent implements OnInit {
 
-  constructor() { }
+  article = {
+    name: '',
+    description: '',
+    available: false};
+
+  submitted = false;
+
+  constructor(private articleService: ArticleService) { }
 
   ngOnInit(): void {
   }
 
+  createArticle(): void {
+    const data = {
+      name: this.article.name,
+      description: this.article.description
+    };
+
+    this.articleService.AddNew(data)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.submitted = true;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  newArticle(): void {
+    this.submitted = false;
+    this.article = {
+      name: '',
+      description: '',
+      available: false
+    };
+  }
+
 }
+
+
