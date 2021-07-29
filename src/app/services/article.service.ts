a2fb0b9270e29373d50e68f45909f50677cd9ae0
Lina Meddeb <lina.meddeb@esprit.tn>
2021-07-29 17:36:51 +0100

    Update the article's service

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+
+const baseURL = 'http://localhost:8080/api/articles';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  GetAll(): Observable<any> {
+    return this.httpClient.get(baseURL);
+  }
+
+  GetById(id): Observable<any> {
+    return this.httpClient.get(`${baseURL}/${id}`);
+  }
+
+  AddNew(data): Observable<any> {
+    return this.httpClient.post(baseURL, data);
+  }
+
+  update(id, data): Observable<any> {
+    return this.httpClient.put(`${baseURL}/${id}`, data);
+  }
+
+  deleteById(id): Observable<any> {
+    return this.httpClient.delete(`${baseURL}/${id}`);
+  }
+
+  deleteAll(): Observable<any> {
+    return this.httpClient.delete(baseURL);
+  }
+
+  searchByName(name): Observable<any> {
+    return this.httpClient.get(`${baseURL}?name=${name}`);
+  }
 }
