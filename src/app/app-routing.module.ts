3a0d0d5904133e1e1a9bfd93ee1a945da7c069e5
Lina Meddeb <lina.meddeb@esprit.tn>
2021-07-29 17:27:43 +0100

    Updating the routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ArticlesListComponent} from "./components/articles-list/articles-list.component";
+import {ArticleDetailsComponent} from "./components/article-details/article-details.component";
+import {ArticleNewComponent} from "./components/article-new/article-new.component";
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'articles', pathMatch: 'full' },
+  { path: 'articles', component: ArticlesListComponent },
+  { path: 'article/:id', component: ArticleDetailsComponent },
+  { path: 'new', component: ArticleNewComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
