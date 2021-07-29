0034673ecc446239f1d9ce6ff6f0b97efd4a4a15
Lina Meddeb <lina.meddeb@esprit.tn>
2021-07-29 17:19:13 +0100

    Components and Service generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleNewComponent } from './components/article-new/article-new.component';
+import { ArticlesListComponent } from './components/articles-list/articles-list.component';
+import { ArticleDetailsComponent } from './components/article-details/article-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleNewComponent,
+    ArticlesListComponent,
+    ArticleDetailsComponent
   ],
   imports: [
     BrowserModule,
